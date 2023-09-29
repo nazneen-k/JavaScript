9967dbdfaaa32a0de92277a75f6fd23da1c5ff8f
Nazneen K <117660793+nazneen-k@users.noreply.github.com>
2023-09-29 09:58:04 +0530

    Add files via upload

---
+++ Objects/script.js
@@ -0,0 +1,66 @@
+/*Objects
+*/
+
+
+// This is an Array --> var a=["Ram","Kumar",18,"India"]
+
+/* 
+This is Object, it has properties and Values
+Objects has names for its Index
+var a={
+    firstName:"Ram",
+    lastName:"Kumar",
+    age:18,
+    country:"India"
+}; 
+
+
+
+
+The name:values pairs in JavaScript objects are called properties:
+Property	Property Value
+firstName	John
+lastName	Doe
+age	50
+eyeColor	blue
+
+
+
+You can access object properties in two ways:
+
+objectName.propertyName
+or
+objectName["propertyName"] 
+
+Objects can also have methods.
+Methods are actions that can be performed on objects.
+Methods are stored in properties as function definitions.
+
+A method is a function stored as a property.
+*/
+
+var a={
+    firstName:"Ram",
+    lastName:"Kumar",
+    age:18,
+    country:"India",
+    favMovies:['Jawaan','Pathaan','Raees'],
+    living: {
+        'city':'Chandigarh',
+        'country':'India'
+    },
+
+    salary: function(){
+        return 25000;
+    },
+    fulllname:function(){
+        return this.firstName+" "+ this.lastname  // this is used here inorder to utilize the properties,without using this it gives error
+    }
+};
+
+document.write(a+"<br>") //---> It Prints the output as [object Object] 
+console.log(a)
+console.log(a.firstName)
+document.write(a.firstName+"<br>")
+document.write(a.salary()+"<br>")
+document.write(a.living.city)
