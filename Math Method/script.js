6a7fc1ad38f6b4a94697dacf9e3681ea001d0ed0
Nazneen K <117660793+nazneen-k@users.noreply.github.com>
2023-10-02 09:53:20 +0530

    Add files via upload

---
+++ Math Method/script.js
@@ -0,0 +1,72 @@
+/*Math Method
+
+-ceil(x)
+-floor(x)
+-round(x)
+-trunc(x)
+-max(x,y,... z)
+-min(x,y,....z)
+-sqrt(x)
+-cbrt(x)
+-pow(x,y)
+-random()
+-abs(x)
+-PI
+
+*/
+
+
+//Math.ceil()
+//It gives the Upper Value
+var a=Math.ceil(5.2)
+document.write(a+"<br>")
+
+//Math.floor()
+//It gives the Lower Value
+var a=Math.floor(4.7)
+document.write(a+"<br>")
+
+//Math.round()
+//It returns the nearest interger value
+var a=Math.round(5.2)
+document.write(a+"<br>")
+
+//Math.trunc()
+//It returns only the integer Value no matter what the decimal Number is 
+var a=Math.trunc(4.5)
+document.write(a+"<br>")
+
+//Math.max()
+//It returns the highest number amongst the others
+var a=Math.max(2,435,54356,432,23)
+document.write(a+"<br>")
+
+//Math.min()
+//It returns the lowest number amongst others
+var a=Math.min(488,43,45,24,56,3451)
+document.write(a+"<br>")
+
+//sqrt()
+var a=Math.sqrt(4)
+document.write(a+"<br>")
+
+//cbrt() --> Cubic Root
+var a=Math.cbrt(8)
+document.write(a+"<br>")
+
+//pow(x,y)
+var a=Math.pow(2,3)
+document.write(a+"<br>")
+
+//random()
+var a=(Math.floor(Math.random()*10))
+document.write(a+"<br>")
+
+//abs()
+var a=Math.abs(5.25)
+document.write(a+"<br>")
+
+//PI
+var a=Math.PI;
+document.write(a+"<br>")
+
