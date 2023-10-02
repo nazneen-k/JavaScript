55351a50ab751a51bb0743ab0f2898328346593b
Nazneen K <117660793+nazneen-k@users.noreply.github.com>
2023-10-02 21:55:24 +0530

    Add files via upload

---
+++ DOM/script.js
@@ -0,0 +1,29 @@
+/*DOM Targeting Methods
+
+-id         --->document.getElementById(id)
+-Class Name --->document.getElementsByClassName(name)
+-Tag Name   --->document.getElementsByTagName(name)
+*/
+
+
+//document
+
+var ele;
+ele=document
+console.log(ele)
+
+ele=document.all; // document.all[2] ---> It returns the title element from the DOM Tree.
+console.log(ele)
+
+//document.head
+//document.title
+//document.body
+//document.links ---> Also returns the nos of elements present in the document
+//document.link[2]
+//document.images
+//document.images[3]
+//document.forms
+//document.doctype
+//document.url
+//document.domain
+//document.baseURI
