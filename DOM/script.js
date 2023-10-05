225ac404495254266aca418ea4dee8ff51125661
Nazneen K <117660793+nazneen-k@users.noreply.github.com>
2023-10-05 12:51:28 +0530

    Update script.js

--- DOM/script.js
+++ DOM/script.js
@@ -8,12 +8,12 @@
 
 //document
 
-var ele;
-ele=document
-console.log(ele)
+// var ele;
+// ele=document
+// console.log(ele)
 
-ele=document.all; // document.all[2] ---> It returns the title element from the DOM Tree.
-console.log(ele)
+// ele=document.all; // document.all[2] ---> It returns the title element from the DOM Tree.
+// console.log(ele)
 
 //document.head
 //document.title
@@ -59,3 +59,87 @@ Using get we can get the : HTML, Text and Attribute.
 
 
 /*DOM querySelector() & querySelectorAll() */
+
+/*
+-querySelector() - It targets only the first selector
+*/
+
+var element;
+
+document.querySelector("#header").innerHTML = "<h1>Wow</h1>";
+
+// element = document.querySelector("#header").getAttribute("class");
+// console.log(element)
+
+
+//querySelectorAll
+
+element = document.querySelectorAll('.list')[1]
+console.log(element)
+
+
+/*DOM CSS Styling Methods */
+
+/*
+-style
+-className
+-classList
+
+ */
+
+
+var e
+document.querySelector("#header").style.backgroundColor="Red" //In order to set the value8/4
+e=document.querySelector("#header").style.border //In order to get the CSS
+console.log(e)
+
+
+
+/*addEventListener Method */
+
+document.getElementById('header').onclick = abc;
+
+function abc(){
+    document.getElementById('header').style.background="green"
+}
+
+
+document.getElementById('header').onmouseenter = abc;
+
+function abc(){
+    document.getElementById('header').style.background="green"
+}
+
+//Next Method
+
+document.getElementById('sidebar').addEventListener("mouseenter",abcd)
+
+function abcd(){
+    document.getElementById('sidebar').style.background="#B0D9B1"
+}
+
+//We can add multiple functions
+
+/*
+OR
+
+document.getElementById('sidebar').addEventListener("click",abc(){ 
+    Statement
+    .
+    .
+    .
+}
+
+*/
+
+
+/*
+Other 
+
+addEventListener(event,function,useCapture)
+
+useCapture--> True or False*/
+
+
+/*
+removeEventListener("ondbclick",functionName) */
