86f8a7e6a43d26ad131cb5b9a1d7d67238491384
Nazneen K <117660793+nazneen-k@users.noreply.github.com>
2023-12-03 19:12:14 +0530

    Update script.js

--- This Keyword/script.js
+++ This Keyword/script.js
@@ -1,119 +1,63 @@
-const user = {
-  name: "Naz",
-  age: 0,
-};
-
-console.log(user.name); //---> Accessing the property
-
-user.name = "Nazneen K"; //---> Modifying the property
-console.log(user.name);
-
-delete user.age; //---> Deleting the property
-console.log(user);
-
-//-----------------------------------------------
-//What will be output of this?
-/*
-const func = (func(a){
-    delete a;
-    return a;
-}) (5)
-console.log(func)
-
-*/
-
-const func = (function (a) {
-  delete a;
-  return a;
-})(5);
-console.log(func); //---> Output: 5
-
-/*
--  Here a is a local variable
-- Delete keyword is only used when we want to delete properties from an object and not a local variable
-- So its not going to effect at all */
-
-//---------------------------------------------------
-
-//Using the keys which will have spaces in between then use "" and while accesssing square brackets[]
-
-const spacedKey = {
-  user: "Naz",
-  "like this spaced keys": true,
-};
-
-console.log(spacedKey["like this spaced keys"]);
-
-//-----------------------------------------------------
-
-//Dynamically addding values
-
-const property = "firstName";
-const name = "Piyush Agarwal";
-
-const users = {
-  [property]: name,
-};
-
-console.log(users);
-
-//--------------------------------------------------------
-//Looping through Objects
-const userss = {
-  name: "Roadside Coder",
-  age: 24,
-  isTotallyAwesome: true,
-};
-
-// We are using for in loop
-
-for (ke in userss) {
-  console.log(ke);
-} //---> for getting the key
-
-for (val in userss) {
-  console.log(userss[val]);
+//Question 1 - What's the Output?
+const user={
+    firstName:"Piyush",
+    getName(){
+        const firstName="Piyush Agarwal"
+        return this.firstName;
+    },
 }
 
-//---------------------------------------------------
-// Question 1 - What is the Output of this?
+console.log(user.getName())
+//Output ---> Piyush
+//This keyword references to the Objects
 
-const obj = {
-  a: "one",
-  b: "two",
-  a: "three",
-};
+//--------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------
+//Question 2 - What is the result of accessing  its ref? Why?
 
-console.log(obj);
-/*
-{a: 'three', b: 'two'}
-a: "three"
-b: "two"
-[[Prototype]]: Objectconstructor: ƒ Object()hasOwnProperty: ƒ hasOwnProperty()isPrototypeOf: ƒ isPrototypeOf()propertyIsEnumerable: ƒ propertyIsEnumerable()toLocaleString: ƒ toLocaleString()toString: ƒ toString()valueOf: ƒ valueOf()__defineGetter__: ƒ __defineGetter__()__defineSetter__: ƒ __defineSetter__()__lookupGetter__: ƒ __lookupGetter__()__lookupSetter__: ƒ __lookupSetter__()__proto__: (...)get __proto__: ƒ __proto__()set __proto__: ƒ __proto__()
-
-- Here a as a key is two times, the first one holds the value as 'one' 
-where as the second one has the value as 'three' the value gets updated
-
-*/
-
-//-------------------------------------------------------
-//Question 2 - Create a function multiplyByTwo(obj) that
-//             multiplies all numeric property value of nums by 2
-
-let nums = {
-  a: 100,
-  b: 200,
-  title: "my name",
-};
-
-multiplyByTwo(nums);
-
-function multiplyByTwo(obj) {
-  for (key in obj) {
-    if (typeof obj[key] === "number") {
-      obj[key] = obj[key] * 2;
+function makeUser(){
+    return{
+        name:"John",
+        ref:this,
     }
-  }
 }
 
-console.log(nums);
+let userr = makeUser();
+console.log(userr)
+
+
+//--------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------
+//Question 3 Whats the output?
+const userrr={
+    name:"Piyush agarwal",
+    logMessage(){
+        console.log(this.name)
+    }
+}
+
+setTimeout(user.logMessage,1000)
+
+
+//--------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------
+//Question 5 - Create an  Object Calculator
+
+let calculator={
+    read(){
+        this.a=+prompt("a=",0)
+        this.b=+prompt("b=",0)
+    },
+
+    sum(){
+        return this.a+this.b;
+    },
+
+    mul(){
+        return this.a*this.b;
+    }
+}
+
+calculator.read()
+console.log(calculator.sum())
+console.log(calculator.mul())
